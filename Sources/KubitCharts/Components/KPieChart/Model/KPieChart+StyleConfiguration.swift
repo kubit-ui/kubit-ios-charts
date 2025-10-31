f179ad07bfefdafd16ec8850bc66c101988f8ea7
larodriguezcastelli <leorcastelli@gmail.com>
2025-10-31 12:00:59 +0100

    Refactor GSPieView (#5)

--- Sources/KubitCharts/Components/KPieChart/Model/KPieChart+StyleConfiguration.swift
+++ Sources/KubitCharts/Components/KPieChart/Model/KPieChart+StyleConfiguration.swift
@@ -9,45 +9,49 @@ extension KPieChart {
         var fontColor: Color?
         /// Color of the background component.
         var backgroundColor: Color?
-        /// Width of the border of the component.
-        var borderWidth: CGFloat
-        /// Color of the inner border.
-        var innerBorderColor: Color?
+        /// Width segment of the component.
+        var lineWidth: CGFloat
         /// Separator color of the component.
         var separatorColor: Color?
+        /// Separator width of the component.
+        var separatorWidth: CGFloat
         /// Bool to show the full or half pie chart.
         var isHalfPieChart: Bool
         /// Padding to apply in the bottom of icon/label.
         var bottomContentPadding: CGFloat
+        /// Bool to show donut or full width chart.
+        var isDonut: Bool
 
-        /// Visual configuration of the component
+        /// Visual configuration of the component.
         ///
         /// - Parameters:
         ///   - font: font style to be shown on the component.
-        ///   - fontColor: color of the font.
+        ///   - fontColor: color of the font. `nil` by default.
         ///   - backgroundColor: color of the background component. `nil` by default.
-        ///   - borderWidth: width of the border of the component. `1` by default.
-        ///   - innerBorderColor: color of the inner border. `nil` by default.
+        ///   - lineWidth: width segment of the component. `1` by default.
         ///   - separatorColor: separator color of the component. `nil` by default.
-        ///   - isHalfPieChart: bool to show the complete pie chart or half pie chart.
-        ///   `false` by default.
+        ///   -  separatorWidth: separator width of the component. `1` by default.
+        ///   - isHalfPieChart: bool to show the complete pie chart or half pie chart. `false` by default.
         ///   - bottomContentPadding: padding to apply at the bottom of icon/label.`0` by default.
+        ///   - isDonut: bool to show donut or full width chart. `false` by default.
         init(font: Font,
              fontColor: Color? = nil,
              backgroundColor: Color? = nil,
-             borderWidth: CGFloat = 1,
-             innerBorderColor: Color? = nil,
+             lineWidth: CGFloat = 1,
              separatorColor: Color? = nil,
+             separatorWidth: CGFloat = 1,
              isHalfPieChart: Bool = false,
-             bottomContentPadding: CGFloat = 0) {
+             bottomContentPadding: CGFloat = 0,
+             isDonut: Bool = false) {
             self.font = font
             self.fontColor = fontColor
             self.backgroundColor = backgroundColor
-            self.borderWidth = borderWidth
-            self.innerBorderColor = innerBorderColor
+            self.lineWidth = lineWidth
             self.separatorColor = separatorColor
+            self.separatorWidth = separatorWidth
             self.isHalfPieChart = isHalfPieChart
             self.bottomContentPadding = bottomContentPadding
+            self.isDonut = isDonut
         }
     }
 }
